86db9dfc6ae6b78d847c6f540a9804546acd50b4
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-10-22 15:21:30 -0600

    added comments to the code

--- env.go
+++ env.go
@@ -6,6 +6,12 @@ import (
 	"strconv"
 )
 
+// Parse is the main function of this package. It takes any valid interface{}
+// value as parameter and expects it to be a pointer to any structure.
+// It attempts to read the 'env' and 'envDefault'
+//
+// An error is returned if the value passed as parameter is not a pointer to a
+// structure and it returns an InvalidInterfaceError.
 func Parse(i interface{}) error {
 	if isInvalidInterface(i) {
 		return InvalidInterfaceError
@@ -15,6 +21,8 @@ func Parse(i interface{}) error {
 	return setStructValues(&elem)
 }
 
+// isInvalidInterface determines whether i is a pointer to a structure.
+// Returns true if it's not a pointer to a structure.
 func isInvalidInterface(i interface{}) bool {
 	if i == nil {
 		return true
@@ -32,6 +40,9 @@ func isInvalidInterface(i interface{}) bool {
 	return interfaceKind != reflect.Struct
 }
 
+// setStructValues iterates over each of the structures elements to determine
+// what value must be set to the field. If no env value of default env value
+// was found, then we skip the current field.
 func setStructValues(structElem *reflect.Value) error {
 	numFields := structElem.NumField()
 	structType := structElem.Type()
@@ -62,6 +73,12 @@ func setStructValues(structElem *reflect.Value) error {
 	return nil
 }
 
+// getEnvOrDefaultValue attempts to read the env tags from the structure field,
+// and returns the environment value found or the default value.
+//
+// Two env tags can be set, and those are the 'env' tag and the 'envDefault'
+// tags. The 'env' tag contains the name of the environment key, and the
+// 'envDefault' tag contains the default value for the structure field.
 func getEnvOrDefaultValue(field *reflect.StructField) string {
 	envKey := field.Tag.Get("env")
 	defaultValue := field.Tag.Get("envDefault")
@@ -79,6 +96,19 @@ func getEnvOrDefaultValue(field *reflect.StructField) string {
 	return envValue
 }
 
+// setValue function is in charge of determining if the field is an
+// exported field (accessable). We return a FieldMustBeAssignableError if
+// the field is unaccessable.
+//
+// For each of the supported types, we just parse the string value into the
+// corresponding type and assigned it to the field. If there's an error when
+// parsing a type, then we return that error.
+//
+// For structures, we recursively call the Parse function in this package
+// to attempt to set all values.
+//
+// If an unsupported field is found, then we return an
+// UnsupportedFieldKindError.
 func setValue(field *reflect.Value, fieldName string, envValue string) error {
 	if !field.CanSet() {
 		return FieldMustBeAssignableError(fieldName)
