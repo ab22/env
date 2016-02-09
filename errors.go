24cc5b47d9d6505b7e2c17ba989615f967bf6b8a
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2016-02-09 10:51:01 -0600

    added comments to errors

--- errors.go
+++ errors.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 )
 
+// ErrInvalidInterface is returned when the value passed to the Parse function
+// is not a pointer to a struct.
 var ErrInvalidInterface = errors.New("env: struct parsing: expected pointer to struct")
 
+// ErrUnsupportedFieldKind is returned when the structure contains an
+// unsupported variable type. Currently, only string, int32, bool, float32 are
+// supported. Also, recursiveness is supported now.
 type ErrUnsupportedFieldKind struct {
 	FieldName string
 	FieldKind string
@@ -16,6 +21,9 @@ func (e ErrUnsupportedFieldKind) Error() string {
 	return fmt.Sprintf("env: set value '%s': unsupported field kind '%s'", e.FieldName, e.FieldKind)
 }
 
+// ErrFieldMustBeAssignable is returned when a field in the structure is
+// unexported (field name starts with lowercase) or there was an error while
+// trying to write to it.
 type ErrFieldMustBeAssignable string
 
 func (field ErrFieldMustBeAssignable) Error() string {
